6507563a3f3816dce902ad96448b281e73edb10c
Vaibhav Ariyur <vaibhav.ariyur@gmail.com>
2020-12-07 13:58:53 -0500

    Lab 5 starting files

--- mipsmem.sv
+++ mipsmem.sv
@@ -1,34 +1,27 @@
 //------------------------------------------------
-// mipsmemsingle.sv
-// David_Harris@hmc.edu 23 October 2005
-// Updated to SystemVerilog dmh 12 November 2010
-// External memories used by MIPS single-cycle
-// processor
+// mipsmem.sv
+// Sarah_Harris@hmc.edu 27 May 2007
+// Update to SystemVerilog 17 Nov 2010 DMH
+// External unified memory used by MIPS multicycle
+// processor.
 //------------------------------------------------
 
-module dmem(input  logic        clk, we,
-            input  logic [31:0] a, wd,
-            output logic [31:0] rd);
+module mem(input  logic        clk, we,
+           input  logic [31:0] a, wd,
+           output logic [31:0] rd);
 
-  logic [31:0] RAM[63:0];
+  logic  [31:0] RAM[63:0];
+
+  // initialize memory with instructions
+  initial
+    begin
+      $readmemh("memfile.dat",RAM);  // "memfile.dat" contains your instructions in hex
+                                     // you must create this file
+    end
 
   assign rd = RAM[a[31:2]]; // word aligned
 
-  always @(posedge clk)
+  always_ff @(posedge clk)
     if (we)
       RAM[a[31:2]] <= wd;
 endmodule
-
-module imem(input  logic [5:0]  a,
-            output logic [31:0] rd);
-
-  logic [31:0] RAM[63:0];
-
-  initial
-    begin
-      $readmemh("memfile2.dat",RAM); // initialize memory
-    end
-
-  assign rd = RAM[a]; // word aligned
-endmodule
-
